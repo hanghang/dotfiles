bc4c876015bcfda1cdb662de37d2d3357f4bd8a2
fw <fw>
2020-09-07 12:49:34 +0800

    add psql to path

--- bin/installer.sh
+++ bin/installer.sh
@@ -15,25 +15,44 @@ git clone git@github.com:hanghang/dotfiles.git
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p dotfiles/vimfiles/gitignored/
+mkdir -p dotfiles/shell/gitignored/
 
 touch ~/dotfiles/vimfiles/gitignored/dbconf.vim
+touch ~/dotfiles/shell/gitignored/ssh
 
+# vim
 if [ -f ~/.vimrc ]; then
-	echo "~/.vimrc already exists, please manually add 'source ~/dotfiles/.vimrc'"
+	echo "~/.vimrc already exists, please manually add 'source ~/dotfiles/vimrc'"
 	err=1;
 else 
-	echo 'source ~/dotfiles/.vimrc' > ~/.vimrc
+	echo 'source ~/dotfiles/vimrc' > ~/.vimrc
 fi
+vim +PlugClean +qall
 
 # tmux
 if [ -f ~/.tmux.conf ]; then
-	echo "~/.tmux.conf already exists, please manually add 'source ~/dotfiles/.tmux.conf'"
+	echo "~/.tmux.conf already exists, please manually add 'source ~/dotfiles/tmux.conf'"
 	err=1;
 else 
-	echo 'source ~/dotfiles/.tmux.conf' > ~/.tmux.conf
+	echo 'source ~/dotfiles/tmux.conf' > ~/.tmux.conf
 fi
 
-vim +PlugClean +qall
+# git
+cd ~
+if [ -f ~/.gitconfig ]; then
+	echo "~/.gitconfig already exists, please manually link 'ln ~/dotfiles/git/gitconfig .gitconfig'"
+	err=1;
+else 
+	ln ~/dotfiles/git/gitconfig .gitconfig
+fi
+if [ -f ~/.gitignore ]; then
+	echo "~/.gitignore already exists, please manually link 'ln ~/dotfiles/git/gitignore .gitignore'"
+	err=1;
+else 
+	ln ~/dotfiles/git/gitignore .gitignore
+fi
+
+
 # result
 if [ !"$err"="0" ]; then
 	echo "Install not complete."
